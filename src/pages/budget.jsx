6e3779243d760aceced3757b9f373809443f0c6f
AngeloRafaelbr <angeloprofissional@gmail.com>
2025-05-31 18:17:39 -0300

    0.4 inicio, sendo 0.2 adicionado componentes, hooks e page relacionado à budget(orçamento) sem alterações aqui - terá que tirar tudo relacionado à email/user e implementar logica do branco de dados

---
+++ src/pages/budget.jsx
@@ -0,0 +1,23 @@
+//import BudgetAlert from "@/components/BudgetAlert/BudgetAlert";
+import BudgetForm from "@/components/BudgetForm/BudgetForm";
+import BudgetList from "@/components/BudgetsList/BudgetList";
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/NavBar/NavBar";
+import styles from "@/styles/budget.module.css"
+
+
+export default function Budget() {
+
+    return (
+            <div className="cont">
+                <Navbar />
+                <div className="main">
+                    <h1 className={styles.budgetTitle}>Gestão de Orçamento</h1>
+                    {/*<BudgetAlert />*/}
+                    <BudgetForm />
+                    <BudgetList />
+                </div>
+                <Footer />
+            </div>
+    )
+}
