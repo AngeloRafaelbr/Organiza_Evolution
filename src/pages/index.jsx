0c55a2b37b9bfa9becfea5f7951e200e9d2a862e
AngeloRafaelbr <angeloprofissional@gmail.com>
2025-05-31 13:48:08 -0300

    Ainda 0.2, com Resume, Form,Grid, Navbar e Footer, porém com retirada de arquivos e codigos desnecessários para essa versão

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,6 @@ import Footer from "@/components/Footer/Footer"
 import Navbar from "@/components/NavBar/NavBar"
 import Home from "@/components/resume/Home"
 
-import PrivateRoute from "@/components/PrivateRoute"
-
 export default function Resume() {
     return (
         
